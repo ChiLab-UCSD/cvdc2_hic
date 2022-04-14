69e07464424d200f82062236c124340642657a9d
decarlin <carlin.daniel@gmail.com>
2022-04-14 10:12:19 -0700

    snp link script

--- hic_handle_scripts/chic_bricks_test.R
+++ hic_handle_scripts/chic_bricks_test.R
@@ -19,8 +19,7 @@ remove_existing = TRUE)
 out_dir <- file.path(tempdir(), "mcool_to_Brick_test")
 My_BrickContainer <- load_BrickContainer(project_dir = out_dir)
 
-mcool_out_dir <- file.path(tempdir(), "mcool_out_dir")
-mcool_path=file.path(mcool_out_dir, "RH_BR_43.mcool")
+#load brick
 
 Brick_load_data_from_mcool(Brick = My_BrickContainer,
 mcool = mcool_path,
@@ -28,19 +27,62 @@ resolution = 10000,
 cooler_read_limit = 10000000,
 matrix_chunk = 2000,
 remove_prior = TRUE,
-norm_factor = "Knight-Ruitz")
+norm_factor = NULL)
 
 mutations<-read.table('results_GroupD5-D15_CM-v-rest_logfc1_redo2.AF_intersect.bed')
 
-search_distance=5e6
+search_distance=2e6
+gene_tbl<-read.table('refGene_hg38.tsv')
+
+extract_tss<-function(row){
+	if (row[4]=='+')
+	{
+		tss<-row[5]
+	}else{
+		tss<-row[6]
+	}
+	tss<-as.numeric(gsub(" ","",tss, fixed=TRUE))
+	return(tss)
+}
+
+all_tss<-apply(gene_tbl,1,extract_tss)
 
 for ( i in 1:dim(mutations)[1])
 {
-	chr=mutations[i,1]
-	start=mutations[i,2]-search_distance
-	end=mutations[i,3]+search_distance
+	chr_chr=mutations[i,1]
+	chr=substring(chr_chr,4,nchar(chr_chr))
+	query_start=mutations[i,2]-search_distance
+	query_end=mutations[i,3]+search_distance
 
-	Brick_fetch_range_index(Brick = My_BrickContainer,
-	chr = chr, start = start, end = end, resolution = 10000)
+	query_range=Brick_fetch_range_index(Brick = My_BrickContainer,
+	chr = chr, start = query_start, end = query_end, resolution = 10000)
+
+	hits<-gene_tbl[which(gene_tbl[,3]==chr_chr & all_tss>query_start & all_tss<query_end),]
+
+	#x coord is the hit
+	query_str<-paste(chr,as.character(query_start),as.character(query_end), sep=':')
+
+	bins<-Brick_return_region_position(Brick = My_BrickContainer,
+    	region = query_str,
+	resolution = 10000)
+
+	Sub_matrix <- Brick_get_matrix_within_coords(Brick = My_BrickContainer,
+    	x_coords=query_str,
+    	force = TRUE,
+    	resolution = 10000,
+    	y_coords = query_str)
 
 }
+
+
+#We will use promoters of Refseq genes extended to a width of 5kb
+#from UCSC browser: http://genome.ucsc.edu/cgi-bin/hgTables
+
+gene_tbl<-read.table('refGene_hg38.tsv')
+
+refseq.db <- makeTxDbFromUCSC(genome="hg38", table="refGene")
+refseq.genes = genes(refseq.db)
+refseq.transcripts = transcriptsBy(refseq.db, by="gene")
+
+refseq.transcripts = refseq.transcripts[ names(refseq.transcripts) %in% unlist(refseq.genes$gene_id) ]
+refseq_promoters <- promoters(refseq.transcripts, 2500,2500)
